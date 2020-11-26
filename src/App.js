3de80e95149009b12a11c0250f3cfe6c9d5ac41d
mjs0818 <tjfaudwo09@gmail.com>
2020-11-27 00:05:02 +0900

    handle callback

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
-import MyCom from './MyCom';
-import MyPureComponent from './MyPureComponent';
+import Counter from './03/Counter2';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.listValue = [{ name: 'Park' }, { name: 'Lee' }];
-    this.state = { version: 0 };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      count: 1,
+    };
   }
-
-  handleClick() {
-    setTimeout(() => {
-      this.listValue[0].name = 'Justin';
-      this.setState({ version: 1 });
-    }, 1000);
-    setTimeout(() => {
-      this.listValue = [{ name: 'Justin' }, { name: 'Lee' }];
-      this.setState({ version: 2 });
-    }, 2000);
+  increaseCount() {
+    this.setState(({ count }) => ({
+      count: count + 1,
+    }));
   }
   render() {
-    console.log(this.listValue);
-    return (
-      <div>
-        <MyCom value={this.listValue} />
-        <MyPureComponent value={this.listValue} />
-        <button onClick={this.handleClick}>버튼</button>
-      </div>
-    );
+    return <Counter count={this.state.count} onAdd={this.increaseCount.bind(this)} />;
   }
 }
