5182b1dbbd421fffa2e0f56495f5fed785a773e1
mjs0818 <tjfaudwo09@gmail.com>
2020-11-26 22:16:33 +0900

    debounce and throttle

--- src/App.js
+++ src/App.js
@@ -4,11 +4,8 @@ import './App.css';
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="title">두잇! 리액트 시작하기</h1>
-      </div>
+      <div className='App'>        <h1 className="title">두잇! 리액트 시작하기</h1></div>
     );
   }
 }
 
-export default App;
